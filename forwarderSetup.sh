f45dda2b382c9c7969af4e60f17c9b5ab940acb3
Clancy Whittle <clancy.whittle@gmail.com>
2019-11-11 15:13:37 +1100

    update forwarder with bind9-utils

--- forwarderSetup.sh
+++ forwarderSetup.sh
@@ -11,7 +11,7 @@ echo "$@" > /tmp/forwarderSetup_params
 #  Install Bind9
 #  https://www.digitalocean.com/community/tutorials/how-to-configure-bind-as-a-caching-or-forwarding-dns-server-on-ubuntu-14-04
 sudo apt-get update -y
-sudo apt-get install bind9 -y
+sudo apt-get install bind9 bind9utils -y
 
 # configure Bind9 for forwarding
 sudo cat > named.conf.options << EndOFNamedConfOptions
